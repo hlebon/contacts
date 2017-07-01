bba70d7c0cfc0bd8944df0c355ed941d33d733b7
hlebon <hanslgarcia@gmail.com>
2017-07-01 00:04:45 -0500

    Adding a Controlled Component
    Se ha agregado un Controlled Component para el search box que se
    implementara.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
 
 
 //const ListContacts = (props) => Component(
-function ListContacts(props) {
+class ListContacts extends Component{
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: ""
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim() })  //actualiza query del state con el valor del input search
+    }
+
+    render(){
         return (
-            <ol className="contact-list">
-                {props.contacts.map(contact => (
-                    <li key={contact.id} className="contact-list-item">
-                        <div className="contact-avatar" style={{backgroundImage: `url(${contact.avatarURL})`}}>
-                        </div>
-                        <div className="contact-list-details">
-                            <p>{contact.name}</p>
-                            <p>{contact.email}</p>
-                        </div>
-                        <button onClick={() => props.onDelete(contact)} className="contact-remove">Remove</button>
-                    </li>
-                ))}
-            </ol>
+            <div className="list-contacts">
+                {JSON.stringify(this.state)}
+                <div className="list-contacts-top">
+                    <input type="text" className="search-contacts" placeholder="Buscar contacto"
+                    value={this.props.query}
+                    onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+                <ol className="contact-list">
+                    {this.props.contacts.map(contact => (
+                        <li key={contact.id} className="contact-list-item">
+                            <div className="contact-avatar" style={{backgroundImage: `url(${contact.avatarURL})`}}>
+                            </div>
+                            <div className="contact-list-details">
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove">Remove</button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
         )
+    }    
 }
 
 
