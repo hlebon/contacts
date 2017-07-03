6dd7ac26d5747e0f7a6758361aa9623aecd47e23
hlebon <hanslgarcia@gmail.com>
2017-07-03 17:00:39 -0500

    fetch data
    se ha agregado la opcion de agregar data desde el exterior y tambien
    anadido las funcionalidad de remove sobre la base de datos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, {Component} from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from "./utils/ContactsAPI";
 
 //const App = (props) => Component
 class App extends Component{
   state = {
-    contacts: [
-    {
-      "id": "ryan",
-      "name": "Ryan Florence",
-      "email": "ryan@reacttraining.com",
-      "avatarURL": "http://localhost:5001/ryan.jpg"
-    },
-    {
-      "id": "michael",
-      "name": "Michael Jackson",
-      "email": "michael@reacttraining.com",
-      "avatarURL": "http://localhost:5001/michael.jpg"
-    },
-    {
-      "id": "tyler",
-      "name": "Tyler McGinnis",
-      "email": "tyler@reacttraining.com",
-      "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-    ]
+    contacts: []
+  }
+
+
+  componentDidMount(){
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState( {contacts} ); //like: {contacts: contacts}
+    })
   }
 
   removeContact = (contact) => {
@@ -32,6 +21,8 @@ class App extends Component{
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
 
+    ConctatsAPI.remove(contact); //remove from database
+
     //if not use this
     //this.setState({})
   }
@@ -39,7 +30,7 @@ class App extends Component{
   render(){
     return (
       <div>
-          <ListContacts onDelete={this.removeContact} contacts={this.state.contacts}/>
+          <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
      </div>
     )
   }
